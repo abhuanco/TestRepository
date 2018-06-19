e758e4fcd06644b366f21155d57789369cac382e
Huanco <rene.huanco.choque@gmail.com>
2018-06-19 10:04:13 -0400

    Change #02#

---
+++ both.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$client = new \Goutte\Client();
+$crawler = $client->request("GET", "http://www.lostiempos.com");
+
+$crawler->filter('.views-field-title a')->each(function ($node) {
+    echo $node->text() . "\n";
+});
